ba15dc9da0301c6281ba0cac98410394dad11999
minindu0 <mw8511@live.com>
2025-03-04 16:02:43 -0600

    benchmark final + data

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ mod bit_tree;
 mod lcsk;
 
 use poa::*;
-use rand::{Rng, SeedableRng, rngs::StdRng};
 use std::{thread, fs::File, io::{self, BufRead}, path::Path};
 use lcsk::{lcsk_pipeline, threaded_lcsk_pipeline};
 use std::time::Instant;
@@ -22,26 +21,34 @@ fn main() {
 fn bench_mark_all () {
     let test_cases = vec![
         ("./data/Synthetic10.fa", 5, 20, 2),
-        //("./data/Synthetic100.fa", 20, 50, 4),
-        //("./data/Synthetic1000.fa", 100, 100, 8),
-        //("./data/Synthetic10000.fa", 200, 100, 12),
-        //("./data/Synthetic30000.fa", 200, 100, 12),
-        //("./data/Synthetic100000.fa", 250, 200, 12),
-        //("./data/Pacbio.fa", 200, 100, 12),
+        ("./data/Synthetic10.fa", 5, 20, 4),
+        ("./data/Synthetic100.fa", 20, 50, 4),
+        ("./data/Synthetic100.fa", 20, 50, 8),
+        ("./data/Synthetic1000.fa", 100, 100, 4),
+        ("./data/Synthetic1000.fa", 100, 100, 8),
+        ("./data/Synthetic10000.fa", 300, 100, 8),
+        ("./data/Synthetic10000.fa", 300, 100, 12),
+        ("./data/Synthetic30000.fa", 300, 100, 8),
+        ("./data/Synthetic30000.fa", 300, 100, 12),
+        ("./data/Synthetic100000.fa", 600, 200, 8),
+        ("./data/Synthetic100000.fa", 600, 200, 12),
+        ("./data/Pacbio.fa", 300, 200, 8),
+        ("./data/Pacbio.fa", 300, 200, 12),
     ];
     
-    for (filename, bandwidth, cut_off, k) in test_cases {
-        let result = bench_mark_one(filename, bandwidth, cut_off, k);
-        // time
-        println!("Time Taken (lcsk, threaded){:?}", filename, result.0);
-        println!("Score (lcsk, threaded){:?}", filename, result.1);
-        println!("Mem Used (lcsk, threaded){:?}", filename, result.2);
+    for (run_index, (filename, bandwidth, cut_off, k)) in test_cases.iter().enumerate() {
+        let result = bench_mark_one(run_index, *filename, *bandwidth, *cut_off, *k);
+        println!("TEST FINAL RESULTS!");
+        println!("{} k:{} > Time Taken (lcsk, threaded) {:?} micro seconds", filename, k, result.0);
+        println!("{} k:{} > Mem used (lcsk, threaded) {:?} Bytes", filename, k, result.1);
+        println!("{} k:{} > Score (lcsk, threaded) {:?}", filename, k, result.2);
     }
 }
 
-fn bench_mark_one (filename: &str, bandwidth: usize, cut_off: usize, k: usize) -> ((usize, usize), (usize, usize), (usize, usize)) {
+fn bench_mark_one (run_index: usize, filename: &str, bandwidth: usize, cut_off: usize, k: usize) -> ((usize, usize), (usize, usize), (usize, usize)) {
     let mut overall_result = ((0, 0), (0, 0), (0, 0));
     let string_vec_vec = get_sequences_from_file(filename);
+    let mut num_of_runs = 0;
     for string_vec in string_vec_vec {
         let result = run_test(string_vec, bandwidth, cut_off, k);
         overall_result.0.0 += result.0.0;
@@ -52,7 +59,20 @@ fn bench_mark_one (filename: &str, bandwidth: usize, cut_off: usize, k: usize) -
 
         overall_result.2.0 += result.2.0;
         overall_result.2.1 += result.2.1;
+        println!("{} {} (score,time,mem)", run_index, num_of_runs);
+        println!("threaded\t{} {} {}", result.2.1, result.0.1, result.1.1);
+        println!("lcsk\t\t{} {} {}", result.2.0, result.0.0, result.1.0);
+        num_of_runs += 1;
     }
+    overall_result.0.0 = overall_result.0.0 / num_of_runs;
+    overall_result.0.1 = overall_result.0.1 / num_of_runs;
+
+    overall_result.1.0 = overall_result.1.0 / num_of_runs;
+    overall_result.1.1 = overall_result.1.1 / num_of_runs;
+
+    overall_result.2.0 = overall_result.2.0 / num_of_runs;
+    overall_result.2.1 = overall_result.2.1 / num_of_runs;
+
     overall_result
 }
 
@@ -108,6 +128,8 @@ fn run_test (string_vec: Vec<String>, bandwidth: usize, cut_off: usize, k: usize
             let section_node_tracker = section_node_trackers[anchor_index].clone();
             children.push(thread::spawn(move || {
                 let mut aligner_child = Aligner::empty(MATCH_SCORE, MISMATCH_SCORE, -GAP_OPEN_SCORE);
+                //use a customized bandwidth depending on the query length and the number of graph nodes;
+                let bandwidth = get_custom_bandwidth(section_query.len());
                 let result = aligner_child.global_simd_banded_threaded_part1 (anchor_start, anchor_index, &section_query, &section_lcsk, bandwidth, section_graph, section_node_tracker);
                 result
             }));  
@@ -124,15 +146,19 @@ fn run_test (string_vec: Vec<String>, bandwidth: usize, cut_off: usize, k: usize
         let threaded_time = now.elapsed().as_micros() as usize;
         // for the total score anchors.len() * match score should be added as anchored at matches
         threaded_score = threaded_score + anchors.len() as i32 * MATCH_SCORE;
-        println!("threaded score {} {}", threaded_score, lcsk_score);
+        //println!("threaded score {} {}", threaded_score, lcsk_score);
         let (obtained_temp_bases, obtained_temp_path) = threaded_aligner.global_simd_banded_threaded_part2 (Alignment {score: threaded_score, operations: all_alignment_ops}, query);
         threaded_all_bases.push(obtained_temp_bases);
         threaded_all_paths.push(obtained_temp_path);
         // results we need
         if index == 2 {
-            threaded_final_score = threaded_score as usize;
-            lcsk_final_score = lcsk_score as usize;
-
+            if threaded_score > 0 {
+                threaded_final_score = threaded_score as usize;
+            }
+            if lcsk_score > 0 {
+                lcsk_final_score = lcsk_score as usize;
+            }
+        
             threaded_time_taken = threaded_time;
             lcsk_time_taken = lcsk_time;
 
@@ -141,6 +167,23 @@ fn run_test (string_vec: Vec<String>, bandwidth: usize, cut_off: usize, k: usize
         }
     }
     ((lcsk_time_taken, threaded_time_taken), (lcsk_memory_used, threaded_memory_used), (lcsk_final_score, threaded_final_score))
+}
+
+fn get_custom_bandwidth (query_len: usize) -> usize {
+    let mut bandwidth = 500;
+    if query_len < 100 {
+        bandwidth = 20;
+    }
+    if query_len < 1000 {
+        bandwidth = 50;
+    }
+    if query_len < 10_000 {
+        bandwidth = 100;
+    }
+    if query_len < 30_000 {
+        bandwidth = 300;
+    }
+    bandwidth
 }
 
 fn get_sequences_from_file (filename: &str) -> Vec<Vec<String>> {
@@ -163,69 +206,4 @@ fn get_sequences_from_file (filename: &str) -> Vec<Vec<String>> {
         }
     }
     result
-}
-
-fn get_random_sequences_from_generator(sequence_length: usize, num_of_sequences: usize, seed: usize) -> Vec<String> {
-    let mut rng = StdRng::seed_from_u64(seed as u64);
-    //vector to save all the sequences 
-    let mut randomvec: Vec<String> = vec![];
-    //generate the first sequence of random bases of length sequence_length
-    let mut firstseq: Vec<char> = vec![];
-    for _ in 0..sequence_length {
-        firstseq.push(match rng.gen_range(0..4) {
-            0 => 'A',
-            1 => 'C',
-            2 => 'G',
-            3 => 'T',
-            _ => 'X'
-        });
-    }
-    //loop for 10 
-    for _ in 0..num_of_sequences {
-        //clone the sequence
-        let mut mutseq = firstseq.clone();
-        //mutate the all the bases with 0.05 chance
-        for i in 0..mutseq.len() {
-            match rng.gen_range(0..20) {
-                0 => {
-                    mutseq[i] = match rng.gen_range(0..4) {
-                        0 => 'A',
-                        1 => 'C',
-                        2 => 'G',
-                        3 => 'T',
-                        _ => 'X'
-                    }
-                },
-                _ => {}
-            }
-        }
-        //put indels at location with chance 0.1 
-        for i in 0..mutseq.len() {
-            let mean_value: f64 = 1.5; //2.0 before
-            //get length of the indel geometric distributed mean value 1.5
-            let indel_length: usize  = ((1.0 - rng.gen::<f64>()).ln() / (1.00 - (1.00 / mean_value) as f64).ln()).ceil() as usize;
-            match rng.gen_range(0..20) {
-                //insertion of elements
-                0 => {
-                    if i + indel_length < mutseq.len() {
-                        for _ in 0..indel_length{
-                            mutseq.insert(i + 1, mutseq[i]);
-                        }
-                    }
-                },
-                //deletion of elements
-                1 => {
-                    if i + indel_length < mutseq.len() {
-                        for _ in 0..indel_length{
-                            mutseq.remove(i);
-                        }
-                    }
-                }
-                _ => {}
-            }
-        }
-        //insert to vector
-        randomvec.push(mutseq.iter().collect::<String>());
-    }
-    randomvec
 }
